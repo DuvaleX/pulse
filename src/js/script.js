de8cc0f05b5719c0cc9136d7dc5e0f1027fa72b9
DuvaleX <falcon.1208.st@gmail.com>
2020-08-25 18:55:02 +0300

    Create carousel

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.svg"></button>',
+        responsive: [{
+                breakpoint: 991,
+                settings: {
+                  dots: false
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }  
+            }
+        ] 
+      });
+  });
